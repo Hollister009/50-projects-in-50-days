21e084da24adc0194f861ac3d0366523f601c639
Hollister009 <reactive.andrew@yahoo.com.ua>
2021-09-25 18:46:00 +0300

    Added debounce functionality for hidden-search

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -6,3 +6,21 @@ btn.addEventListener('click', () => {
   search.classList.toggle('active');
   input.focus();
 });
+
+function onChange(e) {
+  console.log(e.target.value);
+}
+
+const debounce = (fn, ms = 0) => {
+  let timeout;
+
+  return function () {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn.apply(this, arguments);
+    }, ms);
+  };
+};
+
+input.addEventListener('keyup', debounce(onChange, 500));
